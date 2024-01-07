1873c9d0c694e3bdc907513199b8cc0b5dee8824
akbaradiw <akbaradiw000@gmail.com>
2024-01-07 18:28:45 +0700

    add logo on sidebar

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,14 +1,25 @@
 import React from "react"
 import "./style.css";
 import square from "../../assets/squareside.png"
+import dashboard_img from "../../assets/dashboard_logo.png"
+import car_img from "../../assets/car_logo.png"
+import { Link } from "react-router-dom";
 const SideBar = () => {
 
     return (
 <div>
 <div class="sidebar">
+  <div>
   <img src={square} alt="gadogado"/>
-  <a href="#">Dashboard</a>
-  <a href="#">Cars</a>
+  </div>
+  <Link to={"/"}>
+  <img src={dashboard_img} alt="home"/>
+  <a href=""> Dashboard </a>
+  </Link>
+  <Link to={"/car"}>
+  <img src={car_img}  alt="carr"/>
+  <a href="#" id="mobil" >Cars</a>
+  </Link>
 </div>
 </div>
     )
