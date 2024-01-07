be0217a27d73710282a77e8d732f545ca0f1064e
akbaradiw <akbaradiw000@gmail.com>
2024-01-07 19:22:04 +0700

    update car card component

--- src/components/CarCard/index.jsx
+++ src/components/CarCard/index.jsx
@@ -1,8 +1,36 @@
-import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Col, Card, Button, Row, Container} from 'react-bootstrap/';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./style.css";
+
+
 const CarCard = () => {
 
     return (
-        <div>
+  <div className="car-list">
+   
+         <Container >
+         <Row >
+         <Col>
+         <Card >
+           <Card.Img src="" variant="top" alt="jiakhh" />
+           <Card.Body>
+             <Card.Title>Mobil</Card.Title>
+             <Card.Text>
+               Some quick example text to build on the card title and make up the
+               bulk of the card's content.
+             </Card.Text>
+             <Button>Delete</Button>
+             <Button variant="primary">Edit</Button>
+           </Card.Body>
+         </Card>
+         </Col>
+         </Row>
+         </Container>
+        
+   
+   
         </div>
     )
 }
--- src/pages/CarPage/index.jsx
+++ src/pages/CarPage/index.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import SideBar from "../../components/SideBar"
+import CarCard from "../../components/CarCard"
 const CarPage = () => {
 
     return (
         <div>
              <SideBar />
+             <CarCard/>
         </div>
     )
 }
