bfd51a1aea606db5a365750c49373bafa93e9d1b
akbaradiw <akbaradiw000@gmail.com>
2024-01-12 19:01:04 +0700

    add input form in edit page and add page

--- src/pages/AddPage/index.jsx
+++ src/pages/AddPage/index.jsx
@@ -1,6 +1,10 @@
 import React from "react"
 import SideBar from "../../components/SideBar"
 import NavBar from "../../components/NavBar"
+import {Form, Dropdown, DropdownButton, Col, Row} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "./style.css";
+
 
 
 const AddPage = () => {
@@ -9,6 +13,66 @@ const AddPage = () => {
         <div>
             <SideBar />
             <NavBar />
+            <h1> ADD NEW CAR</h1>
+ <div class="add-car">   
+ <Form >
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+        <Form.Label column sm="2">
+          Nama/Tipe Mobil
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="input" placeholder="Input Nama/Tipe Mobil"  />
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+        <Form.Label column sm="2">
+          Harga
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="input" placeholder="Input Harga Sewa Mobil" />
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+        <Form.Label column sm="2">
+          Foto
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="Input" placeholder="Upload Foto Mobil"  />
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+        <Form.Label column sm="2">
+          Kategori
+        </Form.Label>
+        <Col sm="10">
+        <div>
+        <DropdownButton variant="light"
+          title="Pilih Kategori Mobil"
+        >
+          <Dropdown.Item eventKey="1">Action 1</Dropdown.Item>
+          <Dropdown.Item eventKey="2">Action 2</Dropdown.Item>
+        </DropdownButton>
+      </div>
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+        <Form.Label column sm="2">
+          Created At
+        </Form.Label>
+        <Col sm="10">
+          <span>-</span>
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+        <Form.Label column sm="2">
+          Update At
+        </Form.Label>
+        <Col sm="10">
+          <span>-</span>
+        </Col>
+      </Form.Group>
+    </Form>
+    </div>
         </div>
     )
 }
--- src/pages/EditPage/index.jsx
+++ src/pages/EditPage/index.jsx
@@ -1,9 +1,10 @@
 import React from "react"
 import SideBar from "../../components/SideBar"
-import Form from 'react-bootstrap/Form';
+import {Form, Dropdown, DropdownButton, Col, Row} from 'react-bootstrap';
 import "./style.css";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from "../../components/NavBar"
+
 
 
 const EditPage = () => {
@@ -12,19 +13,68 @@ return (
        <div>
  <SideBar/>
  <NavBar/>
+ <h1>EDIT CAR</h1>
+
  <div class="edit">
-<Form>
- <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-  <Form.Label>Email address</Form.Label>
-<Form.Control type="email" placeholder="name@example.com" />
-</Form.Group>
-<Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
- <Form.Label>Example textarea</Form.Label>
- <Form.Control type="email" placeholder="name@example.com" />
- </Form.Group>
- </Form>
- 
-</div>
+ <Form >
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+        <Form.Label column sm="2">
+          Nama/Tipe Mobil
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="input" placeholder="Input Nama/Tipe Mobil"  />
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+        <Form.Label column sm="2">
+          Harga
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="input" placeholder="Input Harga Sewa Mobil" />
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+        <Form.Label column sm="2">
+          Foto
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="Input" placeholder="Upload Foto Mobil"  />
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+        <Form.Label column sm="2">
+          Kategori
+        </Form.Label>
+        <Col sm="10">
+        <div>
+        <DropdownButton variant="light"
+          title="Pilih Kategori Mobil"
+        >
+          <Dropdown.Item eventKey="1">Action 1</Dropdown.Item>
+          <Dropdown.Item eventKey="2">Action 2</Dropdown.Item>
+        </DropdownButton>
+      </div>
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+        <Form.Label column sm="2">
+          Created At
+        </Form.Label>
+        <Col sm="10">
+          <span>-</span>
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+        <Form.Label column sm="2">
+          Update At
+        </Form.Label>
+        <Col sm="10">
+          <span>-</span>
+        </Col>
+      </Form.Group>
+    </Form>
+    </div>
+    
       </div>
     )
 }
