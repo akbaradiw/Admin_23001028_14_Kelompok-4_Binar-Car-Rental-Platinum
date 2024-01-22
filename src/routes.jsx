b007a281b7739cbf803c61fbd84951678dd9380c
Nurfaizzi <ocitbmc@gmail.com>
2024-01-22 13:51:55 +0700

    "Protected Routes "

---
+++ src/routes.jsx
@@ -0,0 +1,50 @@
+import AddPage from "./pages/AddPage";
+import CarPage from "./pages/CarPage";
+import DashboardPage from "./pages/DashboardPage";
+import EditPage from "./pages/EditPage";
+import LoginPage from "./pages/LoginPage";
+import Authentication from "./routes/Authentication";
+import Protected from "./routes/Protected";
+
+export const routes = [
+  {
+    path: "/login",
+    element: (
+      <Authentication>
+        <LoginPage />
+      </Authentication>
+    ),
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <Protected>
+        <DashboardPage />
+      </Protected>
+    ),
+  },
+  {
+    path: "/cars",
+    element: (
+      <Protected>
+        <CarPage />
+      </Protected>
+    ),
+  },
+  {
+    path: "/add",
+    element: (
+      <Protected>
+        <AddPage />
+      </Protected>
+    ),
+  },
+  {
+    path: "/edit/:id",
+    element: (
+      <Protected>
+        <EditPage />
+      </Protected>
+    ),
+  },
+];
---
+++ src/routes/Protected.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router'
+
+const Protected = ({children}) => {
+  const token = localStorage.getItem("accessToken")
+  if(!token){
+    return <Navigate to="/login" />
+  }
+  return (
+    <div>{children || <Outlet />}</div>
+  )
+}
+
+export default Protected
